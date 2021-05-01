c9f246888181d25cf77e41ebea1ba8788d7b5ebb
Julián de Pablo <julidpt98@gmail.com>
2021-05-01 13:34:48 +0200

    fixed register

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const { Pool, Client } = require('pg')
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const sha = require('sha1')
+const date = require('date-and-time')
 
 const app = express();
-const client = new Client({
-    // poner variable virtualenv
-    connectionString: 'postgres://amvnwrnjzqtkgh:e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa@ec2-99-80-200-225.eu-west-1.compute.amazonaws.com:5432/d20hkpogjrcusn',
+const pool = new Pool({
+    user: "amvnwrnjzqtkgh",
+    host: "ec2-99-80-200-225.eu-west-1.compute.amazonaws.com",
+    database: "d20hkpogjrcusn",
+    password: "e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa",
+    port: "5432",
+    connectionString: "postgres://amvnwrnjzqtkgh:e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa@ec2-99-80-200-225.eu-west-1.compute.amazonaws.com:5432/d20hkpogjrcusn",
     ssl: {
         rejectUnauthorized: false
     }
@@ -30,14 +35,14 @@ app.get('/helloworld', (req, res) => {
 
 app.post('/login', async (req, res) => {
     console.log(req)
-    await client.connect()
+    await pool.connect()
     const query = {
         text: 'SELECT * FROM users WHERE users.email = $1 and users.password = $2',
         values: [req.body.email, sha(req.body.password)],
         rowMode: 'array'
     }
-    const result = await client.query(query)
-    client.end()
+    const result = await pool.query(query)
+    pool.end()
 
     if (result.rowCount == 0) {
         return res.status(400).json({
@@ -48,15 +53,16 @@ app.post('/login', async (req, res) => {
     var userid = result.rows[0][0];
     var password = result.rows[0][2];
 
+    const now = new Date();
+    today = date.format(now, 'YYYY-MM-DD')
+
     try{
         if (sha(req.body.password) == password) {
             var claims = {
                 'userid': userid,
-                'exp': Date.now() + 24,
-                'iat': Date.now() //Fecha de creación
+                'exp': today + 24,
+                'iat': today //Fecha de creación
             }
-            console.log(claims.exp)
-            console.log(claims.iat)
         
             const accessToken = jwt.sign(claims, '3ea3967ae8328f89eda5be264d5af88b83d490afc9218d02e5628e07bf89850e828eef80c4085c20e4a394f5a7792773347e7a6492b0e05e54f321a34b7ed20b')
             console.log(accessToken)
@@ -77,20 +83,22 @@ app.post('/login', async (req, res) => {
     // cambiar datetime
 })
 
-app.post('/register', async (req, res) => {
-    console.log(req)
-    // capturar excepcion del select
+app.post('/register', (req, res) => {
     try {
-        await client.connect()
-        const query = {
-            text: 'insert into users (email,password,name,surnames,phone,entry_date,leaving_date,admin,token) values ($1,$2,$3,$4,$5,$6,NULL,false,NULL);',
-            values: [req.body.email, sha(req.body.password), req.body.name, req.body.surnames, req.body.phone, date.getMonth()+1 +'-'+date.getDate()+'-'+date.getFullYear()],
+        console.log(req.body);
+
+        const now = new Date();
+        today = date.format(now, 'YYYY-MM-DD')
+        pool.connect().then(() => console.log("Connected to db"));
+        //var query = "INSERT INTO users (email,password,name,surnames,phone,entry_date,leaving_date,admin,token) VALUES ($1,$2,$3,$4,$5,$6,NULL,false,NULL);",[req.body.email, sha(req.body.password), req.body.name, req.body.surnames, req.body.phone, date.getMonth()+1 +'-'+date.getDate()+'-'+date.getFullYear()],
+        var query = {            
+            text: "INSERT INTO users (email,password,name,surnames,phone,entry_date,leaving_date,admin,token) VALUES ($1,$2,$3,$4,$5,$6,NULL,false,NULL);",
+            values: [req.body.email, sha(req.body.password), req.body.name, req.body.surnames, req.body.phone, today],
             rowMode: 'array'
         }
-        const result = await client.query(query)
-        client.end()
-
-        console.log(result)
+        var result = pool.query(query)
+        pool.end();
+        // var result = pool.query("INSERT INTO users(email,password,name,surnames,phone,entry_date,leaving_date,admin,token) VALUES ($1,$2,$3,$4,$5,$6,NULL,false,NULL);",[req.body.email, sha(req.body.password), req.body.name, req.body.surnames, req.body.phone, today]);
 
         return res.json({
             message: 'ok'
@@ -103,26 +111,26 @@ app.post('/register', async (req, res) => {
 })
 
 app.get('/', (req, res) => {
-    client.query(`SELECT * FROM users;`)
+    pool.query(`SELECT * FROM users;`)
     .then((table) => res.json({ data: table.rows }))
     .catch((err) => res.json(err));
 });
 
 //Middlewares
 
-function authenticateToken(token) {
+async function authenticateToken(token) {
     try {
         decodedToken = jwt.decode(token)
         var userid = decodedToken.userid
 
-        await client.connect()
+        await pool.connect()
         const query = {
             text: 'SELECT token FROM users WHERE users.id_user = $1',
             values: [userid],
             rowMode: 'array'
         }
-        const result = await client.query(query)
-        client.end()
+        const result = await pool.query(query)
+        pool.end()
 
         if(result.body.token != 'NULL') {
             return true
