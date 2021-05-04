ba63e238b057cf3c13e66f448cf06b0b5444dba6
Julián de Pablo <julidpt98@gmail.com>
2021-05-04 22:01:51 +0200

    updated server.js

--- server.js
+++ server.js
@@ -3,21 +3,16 @@ const express = require('express')
 const jwt = require('jsonwebtoken')
 const sha = require('sha1')
 const spawn = require('child_process').spawn
-const d = require('date-and-time')
+const date = require('date-and-time')
 
 const app = express();
-const client = new Client({
-    user: "amvnwrnjzqtkgh",
-    host: "ec2-99-80-200-225.eu-west-1.compute.amazonaws.com",
-    database: "d20hkpogjrcusn",
-    password: "e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa",
-    port: "5432",
+const pool = new Pool({
     connectionString: "postgres://amvnwrnjzqtkgh:e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa@ec2-99-80-200-225.eu-west-1.compute.amazonaws.com:5432/d20hkpogjrcusn",
     ssl: {
         rejectUnauthorized: false
     }
 });
-const scraperAirbnb = spawn('python',["./WebScrapers/airbnb.py", arg1, arg2])
+// const scraperAirbnb = spawn('python',["./WebScrapers/airbnb.py", arg1, arg2])
 
 app.use(express.json())
 app.set('port', process.env.PORT || 8080)
@@ -31,20 +26,21 @@ app.use((req, res, next) => {
 
 app.post('/login', async (req, res) => {
     console.log(req.body)
-    await client.connect()
-    const query = {
-        text: 'SELECT * FROM users WHERE users.email = ? and users.password = ?',
-        values: [req.body.email, sha(req.body.password)],
-        rowMode: 'array'
-    }
-    const result = await client.query(query)
-    client.end(err => {
-        console.log('client has disconnected')
-        if (err) {
-          console.log('error during disconnection', err.stack)
-        }
-    })
-    console.log(result.rows)
+    // const query = {
+    //     text: 'SELECT * FROM users WHERE users.email = ? and users.password = ?',
+    //     values: [req.body.email, sha(req.body.password)],
+    //     rowMode: 'array'
+    // }
+    const query = "SELECT * FROM users WHERE users.email ='" + req.body.email +  "' and users.password = '" + sha(req.body.password) + "';"
+    console.log(query)
+    const result = await pool.query(query)
+    // pool.end(err => {
+    //     console.log('pool has disconnected')
+    //     if (err) {
+    //       console.log('error during disconnection', err.stack)
+    //     }
+    // })
+    console.log(result)
 
     if (result.rowCount == 0) {
         return res.status(400).json({
@@ -52,9 +48,9 @@ app.post('/login', async (req, res) => {
         })
     }
 
-    var userid = result.rows[0][0];
+    var userid = result.rows[0].id_user;
     console.log(userid)
-    var password = result.rows[0][2];
+    var password = result.rows[0].password;
     console.log(password)
 
     try{
@@ -68,23 +64,24 @@ app.post('/login', async (req, res) => {
             const accessToken = jwt.sign(claims, '3ea3967ae8328f89eda5be264d5af88b83d490afc9218d02e5628e07bf89850e828eef80c4085c20e4a394f5a7792773347e7a6492b0e05e54f321a34b7ed20b')
             console.log(accessToken)
 
-            await client.connect()
-            const query = {
-                text: 'UPDATE USERS SET token = ? where id_user = ?',
-                values: [token, userid],
-                rowMode: 'array'
-            }
-            await client.query(query)
-            client.end(err => {
-                console.log('client has disconnected')
-                if (err) {
-                console.log('error during disconnection', err.stack)
-                }
-            })
+            // const query = {
+            //     text: 'UPDATE users SET token = ? where id_user = ?',
+            //     values: [accesToken, userid],
+            //     rowMode: 'array'
+            // }
+            const query = "UPDATE users SET token = '" + accessToken +  "' where id_user = '" + userid + "';"
+            const result2 = await pool.query(query)
+            console.log(result2)
+            // pool.end(err => {
+            //     console.log('pool has disconnected')
+            //     if (err) {
+            //     console.log('error during disconnection', err.stack)
+            //     }
+            // })
 
             return res.json({
                 message: 'ok',
-                token: accesToken
+                token: accessToken
             }); //mirar http valor
         } else {
             return res.json({
@@ -105,14 +102,12 @@ app.post('/register', (req, res) => {
         const now = new Date();
         today = date.format(now, 'YYYY-MM-DD')
 
-        client.connect().then(() => console.log("Connected to db"));
         var query = {            
-            text: "INSERT INTO users (email,password,name,surnames,phone,entry_date,leaving_date,admin,token) VALUES (?,?,?,?,?,?,NULL,false,NULL);",
+            text: "INSERT INTO users (email,password,name,surnames,phone,entry_date,leaving_date,admin,token) VALUES ($1,$2,$3,$4,$5,$6,NULL,false,NULL);",
             values: [req.body.email, sha(req.body.password), req.body.name, req.body.surnames, req.body.phone, today],
             rowMode: 'array'
         }
-        var result = client.query(query)
-        client.end();
+        var result = pool.query(query)
 
         return res.json({
             message: 'ok'
@@ -138,14 +133,14 @@ async function authenticateToken(token) {
         decodedToken = jwt.decode(token)
         var userid = decodedToken.userid
 
-        await client.connect()
+        // await pool.connect()
         const query = {
             text: 'SELECT token FROM users WHERE users.id_user = ?',
             values: [userid],
             rowMode: 'array'
         }
-        const result = await client.query(query)
-        client.end()
+        const result = await pool.query(query)
+        pool.end()
 
         if(result.body.token != 'NULL') {
             return true
