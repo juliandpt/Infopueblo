6d26a25db6791540ac87357bb3d826d9b4b76c00
alfonsovega97 <alfonsovega1005@hotmail.com>
2021-05-07 09:38:10 +0200

    email confirmation register

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const sha = require('sha1')
 const spawn = require('child_process').spawn
 const date = require('date-and-time')
 
+const sendGridMail = require('@sendgrid/mail');
+
 const app = express();
 const pool = new Pool({
     connectionString: "postgres://amvnwrnjzqtkgh:e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa@ec2-99-80-200-225.eu-west-1.compute.amazonaws.com:5432/d20hkpogjrcusn",
@@ -83,21 +85,78 @@ app.post('/login', async (req, res) => {
         })
     }
 })
+app.get('/validate', async (req, res) => {
+    console.log(req.query)
+    // const query = {
+    //     text: 'SELECT * FROM users WHERE users.email = ? and users.password = ?',
+    //     values: [req.body.email, sha(req.body.password)],
+    //     rowMode: 'array'
+    // }
+    const query = "UPDATE users SET validate=true where email = '" + req.query.email + "' and token='" + req.query.token + "';"
+    console.log(query)
+    const result = await pool.query(query)
+    console.log(result)
 
-app.post('/register', (req, res) => {
+    if (result.rowCount == 0) {
+        return res.json({
+            message: 'ko'
+        })
+    }
+    else return res.json({
+        message: 'cuenta validada'
+    })
+})
+
+app.post('/register', async(req, res) => {
     try {
         console.log(req.body);
-
+        email = req.body.email
         const now = new Date();
         today = date.format(now, 'YYYY-MM-DD')
+        var claims = {
+            userid: req.body.name,
+            exp: Date.now(),
+            iat: Date.now() //Fecha de creación
+        }
+    
+        const accessToken = jwt.sign(claims, '3ea3967ae8328f89eda5be264d5af88b83d490afc9218d02e5628e07bf89850e828eef80c4085c20e4a394f5a7792773347e7a6492b0e05e54f321a34b7ed20b')
 
         var query = {            
-            text: "INSERT INTO users (email,password,name,surnames,phone,entry_date,leaving_date,admin,token) VALUES ($1,$2,$3,$4,$5,$6,NULL,false,NULL);",
-            values: [req.body.email, sha(req.body.password), req.body.name, req.body.surnames, req.body.phone, today],
+            text: "INSERT INTO users (email,password,name,surnames,phone,entry_date,leaving_date,admin,token,validate) VALUES ($1,$2,$3,$4,$5,$6,NULL,false,$7,false);",
+            values: [email, sha(req.body.password), req.body.name, req.body.surnames, req.body.phone, today, accessToken],
             rowMode: 'array'
         }
         var result = pool.query(query)
+        sendGridMail.setApiKey('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');
+        url = 'http://127.0.0.1:8080/validate?email='+ email + '&token=' + accessToken
+        function getMessage() {
+        const body = 'Haz click en el siguiente link para validar tu cuenta: ' + url;
+        return {
+            to: email,
+            from: 'correoguapisimo@outlook.com',
+            subject: 'Valida tu cuenta!',
+            text: body,
+            html: `<strong>${body}</strong>`,
+        };
+        }
 
+        async function sendEmail() {
+        try {
+            await sendGridMail.send(getMessage());
+            console.log('Test email sent successfully');
+        } catch (error) {
+            console.error('Error sending test email');
+            console.error(error);
+            if (error.response) {
+            console.error(error.response.body)
+            }
+        }
+        }
+
+        (async () => {
+        console.log('Sending test email');
+        await sendEmail();
+        })();
         return res.json({
             message: 'ok'
         });
@@ -116,7 +175,7 @@ app.post('/search', (req, res)=> {
         
         var jsonContent = JSON.parse(contents)
 
-        res.send(jsonContent[1].title)
+        res.send(jsonContent)
     })
 })
 
