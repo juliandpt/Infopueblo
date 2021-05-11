ad2e96599fb934a947be8ffb4575e977e7e859c9
alfonsovega97 <alfonsovega1005@hotmail.com>
2021-05-11 13:20:01 +0200

    terminado scraper municipios españa

--- WebScrapers/scrapperMunicipios.py
+++ WebScrapers/scrapperMunicipios.py
@@ -1,3 +1,6 @@
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from bs4 import *
 import pandas as pd
 import requests
@@ -5,20 +8,42 @@ import sys
 import time
 import json
 
+con = psycopg2.connect(dbname='d20hkpogjrcusn',
+                       user='amvnwrnjzqtkgh', host='ec2-99-80-200-225.eu-west-1.compute.amazonaws.com',
+                       password='e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa', port='5432')
+
 r = requests.get(f'https://15mpedia.org/w/index.php?title=Especial:Ask&offset=0&limit=8134&q=%5B%5BPage+has+default+form%3A%3AMunicipio%5D%5D+%5B%5Bpa%C3%ADs%3A%3AEspa%C3%B1a%5D%5D&p=format%3Dtable%2Fmainlabel%3DMunicipio&po=%3F%3DMunicipio%23%0A%3FComarca%23-%0A%3FProvincia%0A%3FComunidad+aut%C3%B3noma%3DCC.AA.%0A%3FAltitud%3DAltitud+%28m.s.n.m.%29%0A%3FSuperficie%3DSuperficie+%28km%C2%B2%29%0A%3FPoblaci%C3%B3n+en+2019%3DPoblaci%C3%B3n+%282019%29%0A%3FDensidad+de+poblaci%C3%B3n%3DDensidad+%28hab.%2Fkm%C2%B2%29%0A&sort=nombre&order=asc')
 
 content = r.text
 soup = BeautifulSoup(content, 'html.parser')
 table = soup.find('tbody')
 rows = table.find_all('tr')
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cur = con.cursor()
+i = 0
 
 for row in rows:
   municipio = {}
-  municipio['Municipio'] = row.find('td', {"class": "Municipio"}).text
-  municipio['Comarca'] = row.find('td', {"class": "Comarca"}).text
-  municipio['Provincia'] = row.find('td', {"class": "Provincia"}).text
-  municipio['CC.AA'] = row.find('td', {"class": "CC.AA."}).text
-  municipio['Poblacion'] = row.find('td', {"class": "Población-(2019)"}).text
-  municipio['Densidad(hab/km)'] = row.find('td', {"class": "Densidad-(hab./km²)"}).text
+  i = i +1
+  municipio['Municipio'] = row.find('td', {"class": "Municipio"}).text.replace("'"," ")
+  if row.find('td', {"class": "Comarca"}) is None:
+    municipio['Comarca'] = ''
+  else:
+    municipio['Comarca'] = row.find('td', {"class": "Comarca"}).text.replace("'"," ")
+
+  municipio['Provincia'] = row.find('td', {"class": "Provincia"}).text.replace("'"," ")
+  municipio['CC.AA'] = row.find('td', {"class": "CC.AA."}).text.replace("'"," ")
+  if row.find('td', {"class": "Población-(2019)"}) is None:
+    municipio['Poblacion'] = str(0)
+  else:
+    municipio['Poblacion'] = row.find('td', {"class": "Población-(2019)"}).text.replace('.','')
+  if row.find('td', {"class": "Densidad-(hab./km²)"}) is None:
+    municipio['Densidad(hab/km)'] = str(0)
+  else:
+    municipio['Densidad(hab/km)'] = row.find('td', {"class": "Densidad-(hab./km²)"}).text.replace( '.','').replace(',','.')
+
+  cur.execute("insert into towns (aacc,density_pob,emptied,name,population,province,region) values ('" +
+                    municipio['CC.AA'] + "'," + municipio['Densidad(hab/km)'] + ",NULL,'" + municipio['Municipio'] + "'," + municipio['Poblacion'] + ",'" + municipio['Provincia'] + "','" + municipio['Comarca'] + "');")
+  print('municipio ' + str(i) + ' insertado correctamente')
   print(municipio)
-  break
+cur.close()
