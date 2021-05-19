6a2ca96a60a64c85a025d4b423562228bc083101
alfonsovega97 <alfonsovega1005@hotmail.com>
2021-05-19 19:31:52 +0200

    Update 15mpedia.py

--- WebScrapers/15mpedia.py
+++ WebScrapers/15mpedia.py
@@ -1,23 +1,33 @@
-# import psycopg2
-# from psycopg2 import sql
-# from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from bs4 import *
 import requests
+import mariadb
+import sys
 
-# con = psycopg2.connect(dbname='d20hkpogjrcusn',
-#                        user='amvnwrnjzqtkgh', host='ec2-99-80-200-225.eu-west-1.compute.amazonaws.com',
-#                        password='e795f9d0a48704ea436bbd5d8efdbc914c9e146aaad730b0aca9c819ebbff0aa', port='5432')
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="pr_grupob",
+        password="PC2-2021",
+        host="2.139.176.212",
+        port=3306,
+        database="prgrupob"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+
 
 # r = requests.get(f'https://15mpedia.org/w/index.php?title=Especial:Ask&offset=0&limit=8134&q=%5B%5BPage+has+default+form%3A%3AMunicipio%5D%5D+%5B%5Bpa%C3%ADs%3A%3AEspa%C3%B1a%5D%5D&p=format%3Dtable%2Fmainlabel%3DMunicipio&po=%3F%3DMunicipio%23%0A%3FComarca%23-%0A%3FProvincia%0A%3FComunidad+aut%C3%B3noma%3DCC.AA.%0A%3FAltitud%3DAltitud+%28m.s.n.m.%29%0A%3FSuperficie%3DSuperficie+%28km%C2%B2%29%0A%3FPoblaci%C3%B3n+en+2019%3DPoblaci%C3%B3n+%282019%29%0A%3FDensidad+de+poblaci%C3%B3n%3DDensidad+%28hab.%2Fkm%C2%B2%29%0A&sort=nombre&order=asc')
 
-r = requests.get(f'https://15mpedia.org/w/index.php?title=Especial:Ask&offset=0&limit=5&q=%5B%5BPage+has+default+form%3A%3AMunicipio%5D%5D+%5B%5Bpa%C3%ADs%3A%3AEspa%C3%B1a%5D%5D&p=format%3Dtable%2Fmainlabel%3DMunicipio&po=%3F%3DMunicipio%23%0A%3FComarca%23-%0A%3FProvincia%0A%3FComunidad+aut%C3%B3noma%3DCC.AA.%0A%3FAltitud%3DAltitud+%28m.s.n.m.%29%0A%3FSuperficie%3DSuperficie+%28km%C2%B2%29%0A%3FPoblaci%C3%B3n+en+2019%3DPoblaci%C3%B3n+%282019%29%0A%3FDensidad+de+poblaci%C3%B3n%3DDensidad+%28hab.%2Fkm%C2%B2%29%0A&sort=nombre&order=asc')
+r = requests.get(f'https://15mpedia.org/w/index.php?title=Especial:Ask&offset=0&limit=2&q=%5B%5BPage+has+default+form%3A%3AMunicipio%5D%5D+%5B%5Bpa%C3%ADs%3A%3AEspa%C3%B1a%5D%5D&p=format%3Dtable%2Fmainlabel%3DMunicipio&po=%3F%3DMunicipio%23%0A%3FComarca%23-%0A%3FProvincia%0A%3FComunidad+aut%C3%B3noma%3DCC.AA.%0A%3FAltitud%3DAltitud+%28m.s.n.m.%29%0A%3FSuperficie%3DSuperficie+%28km%C2%B2%29%0A%3FPoblaci%C3%B3n+en+2019%3DPoblaci%C3%B3n+%282019%29%0A%3FDensidad+de+poblaci%C3%B3n%3DDensidad+%28hab.%2Fkm%C2%B2%29%0A&sort=nombre&order=asc')
 
 content = r.text
 soup = BeautifulSoup(content, 'html.parser')
 table = soup.find('tbody')
 rows = table.find_all('tr')
-# con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-# cur = con.cursor()
+cur = conn.cursor()
 i = 0
 
 for row in rows:
@@ -35,10 +45,8 @@ for row in rows:
   soup = BeautifulSoup(content, 'html.parser')
   fullImage = soup.find('div', {"class": "fullImageLink"})
   svgImage = fullImage.find('a')
-  svgLink = requests.get(svgImage['href'])
-  content = svgLink.text
-  soup = BeautifulSoup(content, 'html.parser')
-  print(soup)
+  svgLink = svgImage['href']
+  print(svgLink)
   
   if row.find('td', {"class": "Comarca"}) is None:
     municipio['region'] = ''
