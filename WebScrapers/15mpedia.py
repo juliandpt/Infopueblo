caa686974552673a8d782803ab42b8ca820cc554
alfonsovega97 <alfonsovega1005@hotmail.com>
2021-05-20 13:56:15 +0200

    Update 15mpedia.py

--- WebScrapers/15mpedia.py
+++ WebScrapers/15mpedia.py
@@ -5,6 +5,7 @@ import sys
 
 # Connect to MariaDB Platform
 try:
+<<<<<<< HEAD
   conn = mariadb.connect(
       user="pr_grupob",
       password="PC2-2021",
@@ -16,6 +17,23 @@ try:
 except mariadb.Error as e:
   print(f"Error connecting to MariaDB Platform: {e}")
   sys.exit(1)
+=======
+    conn = mariadb.connect(
+        user="pr_grupob",
+        password="PC2-2021",
+        host="2.139.176.212",
+        port=3306,
+        database="prgrupob"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+
+
+# r = requests.get(f'https://15mpedia.org/w/index.php?title=Especial:Ask&offset=0&limit=8134&q=%5B%5BPage+has+default+form%3A%3AMunicipio%5D%5D+%5B%5Bpa%C3%ADs%3A%3AEspa%C3%B1a%5D%5D&p=format%3Dtable%2Fmainlabel%3DMunicipio&po=%3F%3DMunicipio%23%0A%3FComarca%23-%0A%3FProvincia%0A%3FComunidad+aut%C3%B3noma%3DCC.AA.%0A%3FAltitud%3DAltitud+%28m.s.n.m.%29%0A%3FSuperficie%3DSuperficie+%28km%C2%B2%29%0A%3FPoblaci%C3%B3n+en+2019%3DPoblaci%C3%B3n+%282019%29%0A%3FDensidad+de+poblaci%C3%B3n%3DDensidad+%28hab.%2Fkm%C2%B2%29%0A&sort=nombre&order=asc')
+>>>>>>> parent of 1d6db8a (15mpedia terminado con imagenes)
 
 r = requests.get(f'https://15mpedia.org/w/index.php?title=Especial:Ask&offset=0&limit=2&q=%5B%5BPage+has+default+form%3A%3AMunicipio%5D%5D+%5B%5Bpa%C3%ADs%3A%3AEspa%C3%B1a%5D%5D&p=format%3Dtable%2Fmainlabel%3DMunicipio&po=%3F%3DMunicipio%23%0A%3FComarca%23-%0A%3FProvincia%0A%3FComunidad+aut%C3%B3noma%3DCC.AA.%0A%3FAltitud%3DAltitud+%28m.s.n.m.%29%0A%3FSuperficie%3DSuperficie+%28km%C2%B2%29%0A%3FPoblaci%C3%B3n+en+2019%3DPoblaci%C3%B3n+%282019%29%0A%3FDensidad+de+poblaci%C3%B3n%3DDensidad+%28hab.%2Fkm%C2%B2%29%0A&sort=nombre&order=asc')
 
@@ -66,4 +84,8 @@ for row in rows:
   #                   municipio['aacc'] + "'," + municipio['density'] + ",NULL,'" + municipio['name'] + "'," + municipio['population'] + ",'" + municipio['province'] + "','" + municipio['region'] + "');")
   print('municipio ' + str(i) + ' insertado correctamente')
   print(municipio)
+<<<<<<< HEAD
 # cur.close()
+=======
+# cur.close()
+>>>>>>> parent of 1d6db8a (15mpedia terminado con imagenes)
