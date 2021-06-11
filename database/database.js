338f39164c5517c31bb41fa13d28bae645709575
Juan Lasso de la Vega <45948576+lassete6898@users.noreply.github.com>
2021-06-11 02:18:09 +0200

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,22 +1,21 @@
 const mariadb = require('mariadb')
 require('dotenv').config()
 
-// const pool = mariadb.createPool({
-//     host: process.env.DB_HOST, 
-//     user: process.env.DB_USER, 
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//     connectionLimit: 3
-// });
-
 const pool = mariadb.createPool({
-    host: 'localhost', 
-    user: 'root', 
-    password: '',
-    database: 'prgrupob',
-    connectionLimit: 100
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectionLimit: 3
 });
 
+// const pool = mariadb.createPool({
+//     host: 'localhost', 
+//     user: 'root', 
+//     password: '',
+//     database: 'prgrupob',
+//     connectionLimit: 100
+// });
 
 pool.getConnection((err, connection) => {
     if (err) {
